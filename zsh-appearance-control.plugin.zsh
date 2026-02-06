7a0c1df25e9c3a956392f943e64486233409496e
Andrea Alberti <a.alberti82@gmail.com>
2026-02-06 13:50:08 +0100

    refactor: rework sync/propagate flow around internal cached dark-mode state

--- zsh-appearance-control.plugin.zsh
+++ zsh-appearance-control.plugin.zsh
@@ -7,29 +7,46 @@
 # Create an associative array to organize the variables of this zsh plugin
 typeset -gA _zsh_appearance_control
 _zsh_appearance_control[callback.fnc]=''
+_zsh_appearance_control[dark_mode]=''
+_zsh_appearance_control[needs_sync]=1
+_zsh_appearance_control[needs_init_propagate]=0
+_zsh_appearance_control[last_sync_changed]=0
+_zsh_appearance_control[logon]=0
 
-# propagate from DARK_APPEARANCE -> plugin vars
+# propagate from _zsh_appearance_control[dark_mode] -> plugin vars
 function __my_appearance_propagate() {
   # During shell startup, let plugins initialize first.
-  (( _APPEARANCE_LOGON )) && return 
+  (( _zsh_appearance_control[logon] )) && return
+
+  local dark_mode=${_zsh_appearance_control[dark_mode]:-0}
   
-  if (( DARK_APPEARANCE )); then
-    _zsh_opencode_tab[spinner.bg_hex]="#000000"
-  else
-    _zsh_opencode_tab[spinner.bg_hex]="#FFFFFF"
+  if [[ ${(t)_zsh_opencode_tab} == association* ]]; then
+    if (( dark_mode )); then
+      _zsh_opencode_tab[spinner.bg_hex]="#000000"
+    else
+      _zsh_opencode_tab[spinner.bg_hex]="#FFFFFF"
+    fi
+  fi
+
+  local cb=${_zsh_appearance_control[callback.fnc]}
+  if [[ -n $cb && $+functions[$cb] -eq 1 ]]; then
+    $cb $dark_mode
   fi
 }
 
-# This function returns the OS appearance as the ground truth
-function _get_OS_apperance() {
+# This function returns the OS appearance as the ground truth.
+# It must set REPLY to 0/1.
+function _get_OS_appearance() {
+  local dark_mode
+
   # If in TMUX, we use @dark_appearance as the ground truth
   if [[ -n $TMUX ]]; then
     # Read tmux option (0/1). If unset, default to 0.
     read -r dark_mode < <(tmux show-options -gvq @dark_appearance 2>/dev/null)
     : ${dark_mode:=0}
   else
-    # Fallback outside tmux (use env if present, else default 0)
-    # We need to source it from some local file like in $XDG_CACHE_HOME/something
+    # TODO: source OS appearance outside tmux
+    dark_mode=0
   fi
 
   # Normalize to 0/1 (tmux options may be set as on/true/yes).
@@ -41,51 +58,65 @@ function _get_OS_apperance() {
   REPLY=$dark_mode
 }
 
-function __my_appearance_apply() {
-  local dark_mode
+# Backward-compatible misspelling.
+function _get_OS_apperance() {
+  _get_OS_appearance "$@"
+}
 
-  _get_OS_apperance
+function __my_appearance_sync() {
+  local dark_mode old_mode changed=0
+
+  _get_OS_appearance
   dark_mode=$REPLY
+  old_mode=${_zsh_appearance_control[dark_mode]}
 
-  # Only update env if changed
-  if [[ ${DARK_APPEARANCE:-} != $dark_mode ]]; then
-    export DARK_APPEARANCE=$dark_mode
-  fi
-
-  # Propagate when dirty (USR1 or first prompt) and past logon,
-  # even if DARK_APPEARANCE didn't change.
-  if (( _APPEARANCE_DIRTY )) && (( ! _APPEARANCE_LOGON )); then
+  if [[ $old_mode != $dark_mode ]]; then
+    _zsh_appearance_control[dark_mode]=$dark_mode
+    changed=1
     __my_appearance_propagate
   fi
 
-  _APPEARANCE_DIRTY=0
+  _zsh_appearance_control[last_sync_changed]=$changed
+  _zsh_appearance_control[needs_sync]=0
 }
 
 # Only in interactive shells
 if [[ -o interactive ]]; then
   # Set shell variables for internal appearance state management
-  typeset -g _APPEARANCE_DIRTY=1
-  typeset -g _APPEARANCE_LOGON=1
+  _zsh_appearance_control[needs_sync]=1
+  _zsh_appearance_control[needs_init_propagate]=0
+  _zsh_appearance_control[last_sync_changed]=0
+  _zsh_appearance_control[logon]=1
 
-  # Run one sync early (sets DARK_APPEARANCE), but don't fight plugin init yet
-  __my_appearance_apply
+  # Run one sync early (initializes cache), but don't fight plugin init yet
+  __my_appearance_sync
   
   __my_appearance_precmd() {
-    # First prompt: allow plugins to finish init, then propagate once
-    if (( _APPEARANCE_LOGON )); then
-      _APPEARANCE_LOGON=0
-      _APPEARANCE_DIRTY=1
+    # First prompt: allow plugins to finish init, then propagate once.
+    if (( _zsh_appearance_control[logon] )); then
+      _zsh_appearance_control[logon]=0
+      _zsh_appearance_control[needs_sync]=1
+      _zsh_appearance_control[needs_init_propagate]=1
     fi
 
-    if (( _APPEARANCE_DIRTY )); then
-      __my_appearance_apply
+    if (( _zsh_appearance_control[needs_sync] )); then
+      __my_appearance_sync
+    fi
+
+    if (( _zsh_appearance_control[needs_init_propagate] )); then
+      _zsh_appearance_control[needs_init_propagate]=0
+      if (( ! _zsh_appearance_control[last_sync_changed] )); then
+        __my_appearance_propagate
+      fi
     fi
   }
 
-  precmd_functions+=(__my_appearance_precmd)  
+  if (( ${precmd_functions[(I)__my_appearance_precmd]} == 0 )); then
+    precmd_functions+=(__my_appearance_precmd)
+  fi
 fi
 
-# Signal handler: avoid running tmux/ps/subprocess here. Just mark dirty.
+# Signal handler: avoid running tmux/ps/subprocess here. Just mark needs_sync.
 TRAPUSR1() {
-  _APPEARANCE_DIRTY=1
+  _zsh_appearance_control[needs_sync]=1
 }
